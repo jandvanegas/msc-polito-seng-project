f3ee019088a38df594d675b614f49d76f474b54b
Juan Vanegas <jandvanegas@gmail.com>
2022-05-19 22:29:35 +0200

    feat(api): added service layer for sku resource.

---
+++ code/server/src/service/skuService.js
@@ -0,0 +1,78 @@
+function skuService(skuDao, positionDao) {
+    const getById = (id) => {
+        return skuDao.getById(id);
+    }
+    const getAll = async () => {
+        return await skuDao.getAll();
+    }
+    const add = async (
+        description,
+        weight,
+        volume,
+        notes,
+        price,
+        availableQuantity
+    ) => {
+        return await skuDao.add(
+            description,
+            weight,
+            volume,
+            notes,
+            price,
+            availableQuantity
+        );
+    }
+    const updateById = async (
+        id,
+        newDescription,
+        newWeight,
+        newVolume,
+        newNotes,
+        newPrice,
+        newAvailableQuantity
+    ) => {
+        const sku = await skuDao.getById(id)
+        await skuDao.updateSku(
+            id,
+            newDescription,
+            newWeight,
+            newVolume,
+            newNotes,
+            newPrice,
+            newAvailableQuantity
+        );
+        if (sku.position !== null) {
+            await positionDao.update(
+                sku.position,
+                newWeight,
+                newVolume,
+                sku.weight,
+                sku.volume
+            );
+        }
+    }
+    const updatePosition = async (id, position) => {
+        const sku = await skuDao.getById(id)
+        await skuDao.updatePosition(sku, position)
+        await positionDao.update(sku.position, 0, 0, sku.weight, sku.volume)
+        await positionDao.update(position, sku.weight, sku.volume, 0, 0)
+    }
+
+    const remove = async (id) => {
+        const sku = await skuDao.getById(id)
+        if (sku.position != null) {
+            await positionDao.update(sku.position, 0, 0, sku.weight, sku.volume)
+        }
+    }
+    return {
+        getById: getById,
+        getAll: getAll,
+        add: add,
+        updateById: updateById,
+        updatePosition: updatePosition,
+        remove: remove
+
+    }
+}
+
+module.exports = skuService
